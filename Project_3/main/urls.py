cdec222067d31fd5208bb278db9d2ad0cb70c38a
adrian77720 <adrianszmajewski8@gmail.com>
2025-07-28 14:02:24 +0100

    Created login page with a form for entering username and password, CSRF protection and basic field validation included, Integrated Django messages framework to display login errors, The template follows the site's overall layout

--- Project_3/main/urls.py
+++ Project_3/main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('services_list/', views.services_list, name='services_list'),
     path('', views.index, name='index'),
     path('services/', views.services, name='services'),
+    path('login/', views.login, name='login'),
     ]
--- Project_3/main/views.py
+++ Project_3/main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from .models import Service
 from .models import SubService
 
@@ -20,4 +22,18 @@ def services(request):
     subservice= SubService.objects.all()
     return render(request, 'main/services.html',{'services':services})
 
-
+def login(request):
+    login= Service.objects.all()
+    subservice= SubService.objects.all()
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+ 
+        if user is not None:
+            login(request, user)
+            return redirect('main')  # Redirect to homepage or dashboard
+        else:
+            messages.error(request, 'Invalid username or password.')
+ 
+    return render(request, 'login.html')
