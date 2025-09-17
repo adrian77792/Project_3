b246cb3355892c660ae97647fbfc93ba6cb6b0c9
adrian77720 <adrianszmajewski8@gmail.com>
2025-09-17 07:58:12 +0100

    base dir

--- Project_3/Project_3/settings.py
+++ Project_3/Project_3/settings.py
@@ -57,7 +57,7 @@ LOGOUT_REDIRECT_URL = '/login/'  # Gdzie przekierować po wylogowaniu
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'main' / 'templates'],
+        'DIRS': [BASE_DIR / 'main' / 'templates' / 'main'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
