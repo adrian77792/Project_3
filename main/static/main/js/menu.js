b1475a94e8a6340fcdd1351638c0ea37c0232549
adrian77720 <adrianszmajewski8@gmail.com>
2025-09-28 06:20:27 +0100

    changage files structure for heroku deployment

---
+++ main/static/main/js/menu.js
@@ -0,0 +1,19 @@
+function openMENU() {
+    document.getElementById("hamburger").style.display = "none";
+    const menu=document.getElementById("nav-bottom");
+    menu.classList.add("open");
+}
+
+function closeMENU() {
+    document.getElementById("hamburger").style.display = "block";
+    const menu=document.getElementById("nav-bottom");
+    menu.classList.remove("open");
+}
+
+function scrollToTop() {
+    window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // płynne przewijanie
+    });
+  }
+
