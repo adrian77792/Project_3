2d0ba73109fb8e336f23e53219205d633d80bf59
adrian77720 <adrianszmajewski8@gmail.com>
2025-10-04 07:39:50 +0100

    change menu style

--- Project_3/main/static/main/js/menu.js
+++ Project_3/main/static/main/js/menu.js
@@ -1,12 +1,14 @@
 function openMENU() {
     document.getElementById("hamburger").style.display = "none";
     const menu=document.getElementById("nav-bottom");
+    menu.style.display="flex"
     menu.classList.add("open");
 }
 
 function closeMENU() {
     document.getElementById("hamburger").style.display = "block";
     const menu=document.getElementById("nav-bottom");
+    menu.style.display="none"
     menu.classList.remove("open");
 }
 
