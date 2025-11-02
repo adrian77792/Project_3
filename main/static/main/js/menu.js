efc1e34231306f8407bff5f10538913b072e404b
adrian77720 <adrianszmajewski8@gmail.com>
2025-11-02 07:59:27 +0000

    add close menu button and change menu for better UX

--- main/static/main/js/menu.js
+++ main/static/main/js/menu.js
@@ -2,12 +2,20 @@ function openMENU() {
     //document.getElementById("hamburger").style.display = "none";
     const menu=document.getElementById("nav-bottom");
     menu.classList.add("open");
+    const button=document.getElementById("close-menu");
+    button.classList.remove("mobile-hide");
+    const scrollbutton=document.getElementById("scroll-button");
+    scrollbutton.style.display="none"
 }
 
 function closeMENU() {
     //document.getElementById("hamburger").style.display = "block";
     const menu=document.getElementById("nav-bottom");
     menu.classList.remove("open");
+    const button=document.getElementById("close-menu");
+    button.classList.add("mobile-hide");
+    const scrollbutton=document.getElementById("scroll-button");
+    scrollbutton.style.display="flex";
 }
 
 function scrollToTop() {
@@ -16,4 +24,6 @@ function scrollToTop() {
     behavior: 'smooth' // płynne przewijanie
     });
   }
+
+
 
