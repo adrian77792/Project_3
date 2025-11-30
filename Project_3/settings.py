62a70164437e7e2cf46afb896b1231edd31f86d1
adrian77720 <adrianszmajewski8@gmail.com>
2025-11-30 07:12:29 +0000

    refactor: reorganize Django project settings and structure

--- Project_3/settings.py
+++ Project_3/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ewhdq_ptc2!7hv*^4u+xs7sxa2$o-43&m)4b+q!u1(6wys!nf='
+# SECRET_KEY = 'django-insecure-ewhdq_ptc2!7hv*^4u+xs7sxa2$o-43&m)4b+q!u1(6wys!nf='
 import os
-# SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-1234567890!replace_this'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -119,12 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'main/static/'
+STATIC_URL = '/static/'
 #STATICFILES_DIRS = [BASE_DIR / "static"]
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / "main" / "static",
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
